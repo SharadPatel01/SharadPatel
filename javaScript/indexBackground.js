3bb5aa10fc457eed5032b5a99b9c1389b6063432
sharad <sharad230201@gmail.com>
2022-08-08 10:44:32 +0530

    image replace

--- javaScript/indexBackground.js
+++ javaScript/indexBackground.js
@@ -1,8 +1,20 @@
 const background = document.getElementById("bck");
-for (let i = 0; i < 10; i++) {
-    background.innerHTML += `<span id="span"></span>`;
-    span = document.querySelectorAll(".background span")[i];
-    console.log(Math.floor(Math.random() * 100));
-    span.style.left = Math.floor(Math.random() * 1400) + "px";
-    span.style.top = Math.floor(Math.random() * 1000) + "px";
+if(realWidth<768)
+{
+    for (let i = 0; i < 10; i++) {
+        background.innerHTML += `<span id="span"></span>`;
+        span = document.querySelectorAll(".background span")[i];
+        console.log(Math.floor(Math.random() * 100));
+        span.style.left = Math.floor(Math.random() * 750) + "px";
+        span.style.top = Math.floor(Math.random() * 600) + "px";
+    }
+}
+else{
+    for (let i = 0; i < 10; i++) {
+        background.innerHTML += `<span id="span"></span>`;
+        span = document.querySelectorAll(".background span")[i];
+        console.log(Math.floor(Math.random() * 100));
+        span.style.left = Math.floor(Math.random() * 1400) + "px";
+        span.style.top = Math.floor(Math.random() * 1000) + "px";
+    }
 }
